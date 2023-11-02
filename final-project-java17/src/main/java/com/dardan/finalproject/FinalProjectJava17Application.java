504520588497675801548ace41a2d58727d15db5
dardanqsot <quispesotodaniel@gmail.com>
2023-11-01 22:36:23 -0500

    Project created

---
+++ final-project-java17/src/main/java/com/dardan/finalproject/FinalProjectJava17Application.java
@@ -0,0 +1,13 @@
+package com.dardan.finalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectJava17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalProjectJava17Application.class, args);
+	}
+
+}
---
+++ final-project-java17/src/test/java/com/dardan/finalproject/FinalProjectJava17ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dardan.finalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectJava17ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
